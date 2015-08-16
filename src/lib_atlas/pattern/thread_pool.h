dc21eda2d95ec5652bdef1aea30550a1e863eadb
Thibaut Mattio <thibaut.mattio@gmail.com>
2015-08-16 15:18:35 -0400

    Moving ThreadPool logic into header file. Now complete header-only.

--- src/lib_atlas/pattern/thread_pool.h
+++ src/lib_atlas/pattern/thread_pool.h
@@ -61,6 +61,77 @@ class ThreadPool {
   bool is_stoped_ = {false};
 };
 
+//==============================================================================
+// C / D T O R   S E C T I O N
+
+//------------------------------------------------------------------------------
+//
+ThreadPool::ThreadPool(size_t threads) ATLAS_NOEXCEPT {
+ for (size_t i = 0; i < threads; ++i) {
+  workers_.emplace_back([this] {
+    for (;;) {
+     std::function<void()> task;
+
+     {
+      auto lock = std::unique_lock<std::mutex>{queue_mutex_};
+      condition_.wait(lock,
+                      [this] { return is_stoped_ || !tasks_.empty(); });
+      if (is_stoped_ && tasks_.empty()) {
+       return;
+      }
+      task = std::move(tasks_.front());
+      tasks_.pop();
+     }
+
+     task();
+    }
+  });
+ }
+}
+
+//------------------------------------------------------------------------------
+//
+ThreadPool::~ThreadPool() ATLAS_NOEXCEPT {
+ {
+  auto lock = std::unique_lock<std::mutex>{queue_mutex_};
+  is_stoped_ = true;
+ }
+ condition_.notify_all();
+ for (std::thread &worker : workers_) {
+  worker.join();
+ }
+}
+
+//==============================================================================
+// M E T H O D S   S E C T I O N
+
+//------------------------------------------------------------------------------
+//
+template <class Tp_, class... Args_>
+auto ThreadPool::Enqueue(Tp_ &&f, Args_ &&... args)
+-> std::future<typename std::result_of<Tp_(Args_...)>::type> {
+ using return_type = typename std::result_of<Tp_(Args_...)>::type;
+
+ auto task = std::make_shared<std::packaged_task<return_type()> >(
+     std::bind(std::forward<Tp_>(f), std::forward<Args_>(args)...));
+
+ std::future<return_type> res = task->get_future();
+
+ {
+  auto lock = std::unique_lock<std::mutex>{queue_mutex_};
+
+  // don't allow enqueueing after stopping the pool
+  if (is_stoped_) {
+   throw std::runtime_error("enqueue on stopped ThreadPool");
+  }
+
+  tasks_.emplace([task]() { (*task)(); });
+ }
+
+ condition_.notify_one();
+ return res;
+}
+
 }  // namespace atlas
 
 #endif  // ATLAS_PATTERN_THREAD_POOL_H_
