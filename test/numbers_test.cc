e20305c5ee4f01841ff6088256e99edc80a170a8
SONIA <etienne.b.pilon@gmail.com>
2015-10-27 00:08:59 -0400

    Adding the unit test for the new files

---
+++ test/numbers_test.cc
@@ -0,0 +1,16 @@
+/**
+ * \file	numbers_test.cc
+ * \author	Thibaut Mattio <thibaut.mattio@gmail.com>
+ * \date	26/10/2015
+ * \copyright Copyright (c) 2015 Thibaut Mattio. All rights reserved.
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+#include <gtest/gtest.h>
+#include <lib_atlas/maths/numbers.h>
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
