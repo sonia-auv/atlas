b3d3118dacbe48e5d26d4f5171cfd058356f9dab
Thibaut Mattio <thibaut.mattio@gmail.com>
2015-10-28 23:48:46 -0400

    Unit testing the new features

--- test/timer_test.cc
+++ test/timer_test.cc
@@ -10,6 +10,9 @@
 #include <gtest/gtest.h>
 #include <lib_atlas/sys/timer.h>
 
+using atlas::Timer;
+using atlas::MilliTimer;
+
 TEST(TimerTest, startTimer) {
   atlas::NanoTimer timer;
 
@@ -50,6 +53,53 @@ TEST(TimerTest, unpauseTimer) {
   ASSERT_TRUE(timer.IsRunning());
   ASSERT_GT(value1, 0);
   ASSERT_GT(value2, value1);
+}
+
+/**
+ * Do 100 trials of timing gaps between 0 and 19 milliseconds.
+ * Expect accuracy within one millisecond.
+ */
+TEST(timer_tests, short_intervals) {
+  for (int trial = 0; trial < 100; trial++)
+  {
+    uint32_t ms = rand() % 20;
+    MilliTimer mt(ms);
+    usleep(1000 * ms);
+    int32_t r = mt.Remaining();
+
+    // 1ms slush, for the cost of calling usleep.
+    EXPECT_NEAR(r+1, 0, 1);
+  }
+}
+
+TEST(timer_tests, overlapping_long_intervals) {
+  MilliTimer* timers[10];
+
+  // Experimentally determined. Corresponds to the extra time taken by the loops,
+  // the big usleep, and the test infrastructure itself.
+  const int slush_factor = 14;
+
+  // Set up the timers to each time one second, 1ms apart.
+  for (int t = 0; t < 10; t++)
+  {
+    timers[t] = new MilliTimer(1000);
+    usleep(1000);
+  }
+
+  // Check in on them after 500ms.
+  usleep(500000);
+  for (int t = 0; t < 10; t++)
+  {
+    EXPECT_NEAR(timers[t]->Remaining(), 500 - slush_factor + t, 5);
+  }
+
+  // Check in on them again after another 500ms and free them.
+  usleep(500000);
+  for (int t = 0; t < 10; t++)
+  {
+    EXPECT_NEAR(timers[t]->Remaining(), -slush_factor + t, 5);
+    delete timers[t];
+  }
 }
 
 int main(int argc, char **argv) {
