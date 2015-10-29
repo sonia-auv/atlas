d44bb19f8434998208b878f2062b0c6926af2786
Thibaut Mattio <thibaut.mattio@gmail.com>
2015-10-28 21:22:41 -0400

    Adding unit test for the trigo functions

--- src/lib_atlas/maths/trigo_inl.h
+++ src/lib_atlas/maths/trigo_inl.h
@@ -17,103 +17,37 @@ namespace atlas {
 //
 template <class Tp_>
 ATLAS_ALWAYS_INLINE Tp_ DegToRad(const Tp_ &degrees) ATLAS_NOEXCEPT {
-  constexpr static double conversion_ratio = M_PI / 180;
-  return degrees * conversion_ratio;
+  return degrees * (M_PI / 180);
 }
 
 //------------------------------------------------------------------------------
 //
 template <class Tp_>
 ATLAS_ALWAYS_INLINE Tp_ RadToDeg(const Tp_ &radians) ATLAS_NOEXCEPT {
-  constexpr static double conversion_ratio = 180 / M_PI;
-  return radians * conversion_ratio;
-}
-
-//------------------------------------------------------------------------------
-//
-template <class Tp_>
-ATLAS_ALWAYS_INLINE void PolarToEuler(Tp_ &angle) ATLAS_NOEXCEPT {
-  angle += static_cast<Tp_>(-90) + (angle < static_cast<Tp_>(0)
-                                        ? static_cast<Tp_>(360)
-                                        : static_cast<Tp_>(0));
-}
-
-//------------------------------------------------------------------------------
-//
-template <class Tp_>
-ATLAS_ALWAYS_INLINE void EulerToPolar(Tp_ &angle) ATLAS_NOEXCEPT {
-  angle += static_cast<Tp_>(90) + (angle < static_cast<Tp_>(0)
-                                       ? static_cast<Tp_>(360)
-                                       : static_cast<Tp_>(0));
+  return radians * (180 / M_PI);
 }
 
 //------------------------------------------------------------------------------
 //
 template <typename Tp_>
-ATLAS_ALWAYS_INLINE Tp_ AngleDistRel(const Tp_ &from_angle,
-                                     const Tp_ &to_angle) ATLAS_NOEXCEPT {
-  static Tp_ angle_diff;
+ATLAS_ALWAYS_INLINE Tp_ AngleDistance(const Tp_ &a1,
+                                      const Tp_ &a2) ATLAS_NOEXCEPT {
+  Tp_ angle_diff = a1 - a2;
 
-  angle_diff = from_angle - to_angle;
-
-  // this line does the following:
-  // if c < -180, subtract 360 from a2; else if c > 180, add 360 to a2; else add
-  // 0 to a2
-  // return a2 - angle1
-  return (to_angle +
-          (angle_diff < static_cast<Tp_>(-180)
-               ? static_cast<Tp_>(-360)
-               : (angle_diff > static_cast<Tp_>(180) ? static_cast<Tp_>(360)
-                                                     : static_cast<Tp_>(0)))) -
-         from_angle;
+  if (angle_diff < static_cast<Tp_>(-180)) {
+    angle_diff += static_cast<Tp_>(-360);
+  } else if (angle_diff > static_cast<Tp_>(180)) {
+    angle_diff -= static_cast<Tp_>(360);
+  }
+  return angle_diff;
 }
 
 //------------------------------------------------------------------------------
 //
 template <typename Tp_>
-ATLAS_ALWAYS_INLINE void NormalizeAngle(Tp_ &angle) ATLAS_NOEXCEPT {
-  angle = fmod(angle, static_cast<Tp_>(360));
-}
-
-//------------------------------------------------------------------------------
-//
-// calculates the ratio of difference between a1 and a2 such that angleRatio(x,
-// x+180) = 1 and angleRatio(x, x) = 0
-template <class Tp_>
-ATLAS_ALWAYS_INLINE Tp_ LinearAngleDiffRatio(
-    const Tp_ &from_angle, const Tp_ &to_angle) ATLAS_NOEXCEPT {
-  static Tp_ result;
-  result = fabs(from_angle - to_angle);
-
-  while (result > static_cast<Tp_>(360)) {
-    result -= static_cast<Tp_>(360);
-  }
-  while (result < static_cast<Tp_>(0)) {
-    result += static_cast<Tp_>(360);
-  }
-  if (result > static_cast<Tp_>(180)) {
-    result = static_cast<Tp_>(360) - result;
-  }
-  result /= static_cast<Tp_>(180);
-
-  return result;
-}
-
-//------------------------------------------------------------------------------
-//
-template <class Tp_>
-ATLAS_ALWAYS_INLINE Tp_ TrigAngleDiffRatio(const Tp_ &from_angle,
-                                           const Tp_ &to_angle) ATLAS_NOEXCEPT {
-  static Tp_ from_angle_rad;
-  static Tp_ to_angle_rad;
-  static Tp_ d;
-
-  from_angle_rad = DegToRad(from_angle);
-  to_angle_rad = DegToRad(to_angle);
-  d = sqrt(pow(cos(from_angle_rad) - cos(to_angle_rad), 2) +
-           pow(sin(from_angle_rad) - sin(to_angle_rad), 2));
-
-  return d / static_cast<Tp_>(2);
+ATLAS_ALWAYS_INLINE Tp_ NormalizeAngle(const Tp_ &angle) ATLAS_NOEXCEPT {
+  auto norm = fmod(angle, static_cast<Tp_>(360));
+  return norm < 0 ? 360 + norm : norm;
 }
 
 }  // namespace atlas
--- test/trigo_test.cc
+++ test/trigo_test.cc
@@ -9,6 +9,38 @@
 
 #include <gtest/gtest.h>
 #include <lib_atlas/maths/trigo.h>
+#include <lib_atlas/maths/numbers.h>
+
+using namespace atlas;
+
+TEST(Trigo, angle_conversions) {
+  double a1_deg = 0;
+  double a1_rad = 0;
+  double a2_deg = -572.958;
+  double a2_rad = -10;
+  double a3_deg = 57.2958;
+  double a3_rad = 1.000000358;
+  double a4_deg = 7677.63;
+  double a4_rad = 134.;
+
+  ASSERT_EQ(SetPrecision(DegToRad(a1_deg), 6), SetPrecision(a1_rad, 6));
+  ASSERT_EQ(SetPrecision(RadToDeg(a1_rad), 6), SetPrecision(a1_deg, 6));
+
+  ASSERT_EQ(SetPrecision(DegToRad(a2_deg), 6), SetPrecision(a2_rad, 6));
+  ASSERT_EQ(SetPrecision(RadToDeg(a2_rad), 6), SetPrecision(a2_deg, 6));
+
+  ASSERT_EQ(SetPrecision(DegToRad(a3_deg), 6), SetPrecision(a3_rad, 6));
+  ASSERT_EQ(SetPrecision(RadToDeg(a3_rad), 6), SetPrecision(a3_deg, 6));
+
+  ASSERT_EQ(SetPrecision(DegToRad(a4_deg), 6), SetPrecision(a4_rad, 6));
+  ASSERT_EQ(SetPrecision(RadToDeg(a4_rad), 6), SetPrecision(a4_deg, 6));
+}
+
+TEST(Trigo, normalize) {
+  ASSERT_EQ(SetPrecision(NormalizeAngle(45.5), 6), SetPrecision(45.5, 6));
+  ASSERT_EQ(SetPrecision(NormalizeAngle(-29.), 6), SetPrecision(360.-29, 6));
+  ASSERT_EQ(SetPrecision(NormalizeAngle(720.), 6), SetPrecision(0., 6));
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
