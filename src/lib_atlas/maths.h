ed2ed6410656a890d82457e1cb30c8fe95207351
Thibaut Mattio <thibaut.mattio@gmail.com>
2016-02-20 21:22:09 -0500

    Adding maths.h file at root for including all the files

---
+++ src/lib_atlas/maths.h
@@ -0,0 +1,34 @@
+/**
+ * \file	maths.h
+ * \author	Thibaut Mattio <thibaut.mattio@gmail.com>
+ * \date	20/02/2016
+ *
+ * \copyright Copyright (c) 2015 S.O.N.I.A. All rights reserved.
+ *
+ * \section LICENSE
+ *
+ * This file is part of S.O.N.I.A. software.
+ *
+ * S.O.N.I.A. software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * S.O.N.I.A. software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with S.O.N.I.A. software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIB_ATLAS_MATHS_H_
+#define LIB_ATLAS_MATHS_H_
+
+#include <lib_atlas/maths/matrix.h>
+#include <lib_atlas/maths/numbers.h>
+#include <lib_atlas/maths/stats.h>
+#include <lib_atlas/maths/trigo.h>
+
+#endif  // LIB_ATLAS_MATHS_H_
